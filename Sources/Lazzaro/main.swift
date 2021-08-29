913df206270c38cf9cfc57cc7ceb630814770c6c
Chuy <chuynadamas@gmail.com>
2021-08-29 18:02:43 -0500

    Update name and repo

--- Sources/Lazzaro/main.swift
+++ Sources/Lazzaro/main.swift
@@ -17,9 +17,9 @@ struct Lazzaro: Website {
     }
 
     // Update these properties to configure your website:
-    var url = URL(string: "https://cerovaroproductions.com")!
-    var name = "Cero Varo Productions"
-    var description = "This is the lazaro project, the man who return from dead"
+    var url = URL(string: "https://chuynadamas.com")!
+    var name = "ChuyNadaMas - Project Lazaro"
+    var description = "This is the lazaro project, the blog who return from dead"
     var language: Language { .english }
     var imagePath: Path? { nil }
     
@@ -28,7 +28,7 @@ struct Lazzaro: Website {
 // This will generate your website using the built-in Foundation theme:
 try Lazzaro().publish(
     withTheme: .lazzaro,
-    deployedUsing: .gitHub("chuynadamas/Lazzaro", useSSH: true)
+    deployedUsing: .gitHub("chuynadamas/chuynadamas", useSSH: true)
 )
 
 
